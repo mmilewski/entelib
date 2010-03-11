a379a9fe566508ad18d88a32cabafdaebaabf2d2
brzoskamarek <brzoskamarek@b7c1d20a-2643-11df-8747-af52ddc49692>
2010-03-11 19:31:56 +0000

    url handling passed to baseapp/urls.py

--- entelib/urls.py
+++ entelib/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^entelib/', include('entelib.foo.urls')),
+    (r'^entelib/', include('entelib.baseapp.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
