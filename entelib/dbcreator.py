2d724248b82033ec5b6fbcf934b5b540332b5926
mmilewski@gmail.com <mmilewski@gmail.com@b7c1d20a-2643-11df-8747-af52ddc49692>
2010-04-30 21:33:50 +0000

    Added extra argument to run.sh and dbcreator.py, to create db from scratch.

--- entelib/dbcreator.py
+++ entelib/dbcreator.py
@@ -1,8 +1,21 @@
-#-*- coding=utf-8 -*-  
+#-*- coding=utf-8 -*-
+
+#
+# Pass argument 'new' to create db from scratch.
+#
+
+from os import system
+from sys import argv
+
+db_file = 'database/database.db'
+
+# if argument 'new' passed -> remove db at first
+if len(argv) > 1:
+    if argv[1] == 'new':
+        system('rm -f %s' % db_file)
+
 try:
-    open('database/database.db','r')
-except:
-    from os import system
+    open(db_file, 'r')
+except IOError:
     system('echo "no" | python manage.py syncdb')
     system('echo "import dbfiller" | python manage.py shell')
-
--- entelib/run.sh
+++ entelib/run.sh
@@ -4,5 +4,17 @@
 #  http://docs.djangoproject.com/en/dev/intro/tutorial01/#the-development-server
 #  http://docs.djangoproject.com/en/dev/ref/django-admin/#djadmin-runserver
 
-python dbcreator.py
+# Running ./run.sh new  will recreate db.
+
+if [ $# -gt 0 ]; then
+    if [ $1 = "new" ]; then
+        python dbcreator.py new
+    else
+        echo -e "Given ARGUMENT is INCORRECT. Possible commands:"
+        echo -e "\tnew - recreate database"
+        echo
+        exit 1
+    fi
+fi
+
 python manage.py runserver 8484
