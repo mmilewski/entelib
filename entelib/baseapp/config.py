0d12bef62cf09be4773c2912c9cb147548a22779
mmilewski@gmail.com <mmilewski@gmail.com@b7c1d20a-2643-11df-8747-af52ddc49692>
2010-05-21 22:54:24 +0000

    Implemented saving lists with Config

--- entelib/baseapp/config.py
+++ entelib/baseapp/config.py
@@ -1,4 +1,5 @@
 from entelib.baseapp.models import Configuration
+import json
 
 
 class Config(object):
@@ -37,7 +38,7 @@ class Config(object):
         elif value == False:
             value = Config._false_value
         elif isinstance(value, list):
-            assert(False and 'Config is unable to handle lists')
+            value = json.dumps(value)
         elif isinstance(value, dict):
             assert(False and 'Config is unable to handle dicts')
         else:
@@ -63,18 +64,64 @@ class Config(object):
         except Configuration.DoesNotExist:
             return False
 
+    # str
+    def get_str(self, key):
+        '''
+        Returns unicode string associated with given key.
+        Raises Configuration.DoesNotExist if key isn't found.
+        '''
+        return unicode(self.__getitem__(key))
+
+    def set_str(self, key, value):
+        value = unicode(value)
+        self.__setitem__(key, value)
+
+    # int
     def get_int(self, key):
         '''
         Returns integer associated with given key.
-        Rises Configuration.DoesNotExist if key isn't found.
+        Raises Configuration.DoesNotExist if key isn't found.
+        Raises ValueError iff int(key) does.
         '''
         return int(self.__getitem__(key))
 
+    def set_int(self, key, value):
+        '''
+        Associates integer value with key.
+        Raises ValueError iff int(key) does.
+        '''
+        value = int(value)
+        self.__setitem__(key, value)
+
+    # bool
     def get_bool(self, key):
         '''
         Returns True or False - legal bool value, neither 'True' nor '1'.
-        Rises Configuration.DoesNotExist if key isn't found.
+        Raises Configuration.DoesNotExist if key isn't found.
         '''
         value = self.__getitem__(key)
         assert(value in [Config._true_value, Config._false_value])
         return value == Config._true_value
+
+    def set_bool(self, key, value):
+        value = bool(value)
+        self.__setitem__(key, value)
+
+    # list
+    def get_list(self, key):
+        '''
+        Returns list.
+        Raises Configuration.DoesNotExist if key isn't found.
+        Raises ValueError if value associated with key is not a list.
+        '''
+        value = self.__getitem__(key)
+        value = json.loads(value)
+        return value
+
+    def set_list(self, key, value):
+        '''
+        Associates list with given key.
+        Raises ValueError if value associated with key is not a list.
+        '''
+        value = list(value)
+        self.__setitem__(key, value)   # json.dumps is done in __setitem__
--- entelib/baseapp/tests/dbconfig.py
+++ entelib/baseapp/tests/dbconfig.py
@@ -19,34 +19,54 @@ class DbConfigTest(TestCase):
             ('some_true_value', Config._true_value),
             ('some_false_value', Config._false_value),
             ('some_bool_value', Config._false_value),
+            ('simple_int_list', '[1, 2, 7]'),
+            ('empty_list', '[]'),
             ]
         for k, v in data:
             ConfigModel(key=k, value=v).save()
 
-    def test_get_existing_key(self):
-        self.assertEqual('Disney', self.c['mickey_mouse_creator'])
-
-    def test_setting_value(self):
-        self.c['almost_random_prefix'] = 'tralala'
-        self.assertEqual('tralala', self.c['almost_random_prefix'])
-        self.c['is_42_the_answer'] = True
-        self.assertEqual(Config._true_value, self.c['is_42_the_answer'])  # when using brackets Config doesn't know value's type
-        self.assertEqual(True, self.c.get_bool('is_42_the_answer'))       # so you should use get_bool() while _true_value is private
-        self.assertEqual(False, not self.c.get_bool('is_42_the_answer'))
-
     def test_containing(self):
+        ''' Checks if config contains values set up in setUp method. '''
         self.assertTrue('most_funny_number' in self.c)
         self.assertTrue('some_true_value' in self.c)
         self.assertTrue('some_false_value' in self.c)
+        self.assertTrue('simple_int_list' in self.c)
+        self.assertTrue('empty_list' in self.c)
         self.assertFalse('this_string_is_not_in_config' in self.c)
+
+
+    ''' string tests '''
+    def test_get_string(self):
+        self.assertEqual('Disney', self.c['mickey_mouse_creator'])
+
+    def test_updating_string_with_brackets(self):
+        self.c['mickey_mouse_creator'] = 'Walt Disney'
+        self.assertEqual(unicode, type(self.c.get_str('mickey_mouse_creator')))
+        self.assertEqual('Walt Disney', self.c.get_str('mickey_mouse_creator'))
+
+    def test_updating_string_with_setter(self):
+        self.c.set_str('mickey_mouse_creator', 'Walt Disney')
+        self.assertEqual(unicode, type(self.c.get_str('mickey_mouse_creator')))
+        self.assertEqual('Walt Disney', self.c.get_str('mickey_mouse_creator'))
+        self.assertEqual(unicode, type(self.c['mickey_mouse_creator']))
+        self.assertEqual('Walt Disney', self.c['mickey_mouse_creator'])
+
+    def test_creating_string(self):
+        self.c['almost_random_prefix'] = 'tralala'
+        self.assertEqual('tralala', self.c['almost_random_prefix'])
 
 
     ''' int tests '''
     def test_get_int(self):
         self.assertEqual(8, self.c.get_int('most_funny_number'))
 
-    def test_updating_int(self):
+    def test_updating_int_with_brackets(self):
         self.c['most_funny_number'] = 42
+        self.assertEqual(type(42), type(self.c.get_int('most_funny_number')))
+        self.assertEqual(42, self.c.get_int('most_funny_number'))
+
+    def test_updating_int_with_setter(self):
+        self.c.set_int('most_funny_number', 42)
         self.assertEqual(type(42), type(self.c.get_int('most_funny_number')))
         self.assertEqual(42, self.c.get_int('most_funny_number'))
 
@@ -60,18 +80,56 @@ class DbConfigTest(TestCase):
         self.assertEqual(True, self.c.get_bool('some_true_value'))
         self.assertEqual(False, self.c.get_bool('some_false_value'))
 
-    def test_updating_bool(self):
+    def test_updating_bool_with_brackets(self):
         self.c['some_bool_value'] = True
         self.assertEqual(type(True), type(self.c.get_bool('some_bool_value')))
         self.assertEqual(True, self.c.get_bool('some_bool_value'))
+        #
         self.c['some_bool_value'] = False
+        self.assertEqual(type(False), type(self.c.get_bool('some_bool_value')))
+        self.assertEqual(False, self.c.get_bool('some_bool_value'))
+
+    def test_updating_bool_with_setter(self):
+        self.c.set_bool('some_bool_value', True)
+        self.assertEqual(type(True), type(self.c.get_bool('some_bool_value')))
+        self.assertEqual(True, self.c.get_bool('some_bool_value'))
+        #
+        self.c.set_bool('some_bool_value', False)
         self.assertEqual(type(False), type(self.c.get_bool('some_bool_value')))
         self.assertEqual(False, self.c.get_bool('some_bool_value'))
 
     def test_creating_bool(self):
         self.c['green_apples_exists'] = True
-        self.assertEqual(type(True), type(self.c.get_bool('green_apples_exists')))
+        self.assertEqual(Config._true_value, self.c['green_apples_exists'])           # when using brackets Config doesn't know value's type
+        self.assertEqual(type(True), type(self.c.get_bool('green_apples_exists')))    # so you should use get_bool() while _true_value is private
         self.assertEqual(True, self.c.get_bool('green_apples_exists'))
+        self.assertEqual(False, not self.c.get_bool('green_apples_exists'))
 
 
     ''' list tests '''
+    def test_get_list(self):
+        self.assertEqual([1,2,7], self.c.get_list('simple_int_list'))
+        self.assertEqual([], self.c.get_list('empty_list'))
+
+    def test_updating_list_with_brackets(self):
+        self.c['simple_int_list'] = [5, 25, 256, 20]
+        self.assertEqual(type([]), type(self.c.get_list('simple_int_list')))
+        self.assertEqual([5, 25, 256, 20], self.c.get_list('simple_int_list'))
+        #
+        self.c['simple_int_list'] = []
+        self.assertEqual(type([]), type(self.c.get_list('simple_int_list')))
+        self.assertEqual([], self.c.get_list('simple_int_list'))
+
+    def test_updating_list_with_setter(self):
+        self.c.set_list('simple_int_list', [5, 25, 256, 20])
+        self.assertEqual(type([]), type(self.c.get_list('simple_int_list')))
+        self.assertEqual([5, 25, 256, 20], self.c.get_list('simple_int_list'))
+        #
+        self.c.set_list('simple_int_list', [])
+        self.assertEqual(type([]), type(self.c.get_list('simple_int_list')))
+        self.assertEqual([], self.c.get_list('simple_int_list'))
+
+    def test_creating_list(self):
+        self.c['mixed_list'] = [1, 3.1416,'hello', "world"]
+        self.assertEqual(type([]), type(self.c.get_list('mixed_list')))
+        self.assertEqual([1,3.1416, 'hello', "world"], self.c.get_list('mixed_list'))
