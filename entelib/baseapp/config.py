55ca166af26c649a918f79350bc7a1e419ae16f8
mmilewski@gmail.com <mmilewski@gmail.com@b7c1d20a-2643-11df-8747-af52ddc49692>
2010-05-21 22:07:00 +0000

    Implemented boolean support in Config

--- entelib/baseapp/config.py
+++ entelib/baseapp/config.py
@@ -5,8 +5,20 @@ class Config(object):
     '''
     Accesses configuration stored in database.
     '''
+
+    # private consts
+    _true_value = u'1'           # the way that True is stored in db
+    _false_value = u'0'          # the way that False is stored in db
+
+
     def __init__(self):
         self._cached_data = {}
+
+    def clear(self, truncate_model=False):
+        ''' Clears cache. If truncate_model is set, removes all data from Configuration model. '''
+        self._cached_data = {}
+        if truncate_model:
+            Configuration.object.all().delete()
 
     def __getitem__(self, key):
         if key in self._cached_data:
@@ -20,7 +32,16 @@ class Config(object):
                 raise e
 
     def __setitem__(self, key, value):
-        value = unicode(value)
+        if value == True:
+            value = Config._true_value
+        elif value == False:
+            value = Config._false_value
+        elif isinstance(value, list):
+            assert(False and 'Config is unable to handle lists')
+        elif isinstance(value, dict):
+            assert(False and 'Config is unable to handle dicts')
+        else:
+            value = unicode(value)
         self._cached_data[key] = value
 
         # update
@@ -35,6 +56,7 @@ class Config(object):
             record.save()
 
     def __contains__(self, item):
+        ''' Checks if item is in configuration. Returns True or False. '''
         try:
             self.__getitem__(item)
             return True
@@ -42,4 +64,17 @@ class Config(object):
             return False
 
     def get_int(self, key):
+        '''
+        Returns integer associated with given key.
+        Rises Configuration.DoesNotExist if key isn't found.
+        '''
         return int(self.__getitem__(key))
+
+    def get_bool(self, key):
+        '''
+        Returns True or False - legal bool value, neither 'True' nor '1'.
+        Rises Configuration.DoesNotExist if key isn't found.
+        '''
+        value = self.__getitem__(key)
+        assert(value in [Config._true_value, Config._false_value])
+        return value == Config._true_value
--- entelib/baseapp/tests/dbconfig.py
+++ entelib/baseapp/tests/dbconfig.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.conf import settings
 
 from baseapp.config import Config
-from baseapp.models import Configuration
+from baseapp.models import Configuration as ConfigModel
 
 
 class DbConfigTest(TestCase):
@@ -13,19 +13,35 @@ class DbConfigTest(TestCase):
 
     def setUp(self):
         self.c = Config()
-        record = Configuration(key='most_funny_number', value='8')
-        record.save()
+        data = [
+            ('mickey_mouse_creator', 'Disney'),
+            ('most_funny_number', '8'),
+            ('some_true_value', Config._true_value),
+            ('some_false_value', Config._false_value),
+            ('some_bool_value', Config._false_value),
+            ]
+        for k, v in data:
+            ConfigModel(key=k, value=v).save()
 
     def test_get_existing_key(self):
-        self.assertEqual('8', self.c['most_funny_number'])
+        self.assertEqual('Disney', self.c['mickey_mouse_creator'])
 
     def test_setting_value(self):
         self.c['almost_random_prefix'] = 'tralala'
         self.assertEqual('tralala', self.c['almost_random_prefix'])
+        self.c['is_42_the_answer'] = True
+        self.assertEqual(Config._true_value, self.c['is_42_the_answer'])  # when using brackets Config doesn't know value's type
+        self.assertEqual(True, self.c.get_bool('is_42_the_answer'))       # so you should use get_bool() while _true_value is private
+        self.assertEqual(False, not self.c.get_bool('is_42_the_answer'))
 
     def test_containing(self):
         self.assertTrue('most_funny_number' in self.c)
+        self.assertTrue('some_true_value' in self.c)
+        self.assertTrue('some_false_value' in self.c)
+        self.assertFalse('this_string_is_not_in_config' in self.c)
 
+
+    ''' int tests '''
     def test_get_int(self):
         self.assertEqual(8, self.c.get_int('most_funny_number'))
 
@@ -37,3 +53,25 @@ class DbConfigTest(TestCase):
     def test_creating_int(self):
         self.c['some_int_name'] = 123654
         self.assertEqual(123654, self.c.get_int('some_int_name'))
+
+
+    ''' bool tests '''
+    def test_get_bool(self):
+        self.assertEqual(True, self.c.get_bool('some_true_value'))
+        self.assertEqual(False, self.c.get_bool('some_false_value'))
+
+    def test_updating_bool(self):
+        self.c['some_bool_value'] = True
+        self.assertEqual(type(True), type(self.c.get_bool('some_bool_value')))
+        self.assertEqual(True, self.c.get_bool('some_bool_value'))
+        self.c['some_bool_value'] = False
+        self.assertEqual(type(False), type(self.c.get_bool('some_bool_value')))
+        self.assertEqual(False, self.c.get_bool('some_bool_value'))
+
+    def test_creating_bool(self):
+        self.c['green_apples_exists'] = True
+        self.assertEqual(type(True), type(self.c.get_bool('green_apples_exists')))
+        self.assertEqual(True, self.c.get_bool('green_apples_exists'))
+
+
+    ''' list tests '''
