80e6ba510ef94479669e428696ed25b2a5dcf78b
mmilewski@gmail.com <mmilewski@gmail.com@b7c1d20a-2643-11df-8747-af52ddc49692>
2010-05-22 21:33:13 +0000

    Implemented activating users - see http://127.0.0.1:8484/entelib/admin/baseapp/customuser/

--- entelib/baseapp/admin.py
+++ entelib/baseapp/admin.py
@@ -4,8 +4,32 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from entelib.baseapp.models import Location, State, Publisher, Picture, Author, Book, BookCopy, Reservation, Rental, Phone, PhoneType, CostCenter
 
+
+class CustomUserAdmin(admin.ModelAdmin):
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    list_display = ('username', 'first_name', 'last_name', 'email', 'is_active', 'is_superuser', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    actions = ['activate_users', 'deactivate_users']
+
+    # see /usr/lib/pymodules/python2.6/django/contrib/admin/actions.py
+    # see http://docs.djangoproject.com/en/dev/ref/contrib/admin/actions/
+
+    def activate_users(self, request, queryset):
+        rows_updated = queryset.update(is_active=True)
+        message_bit = "1 account was" if rows_updated == 1 else "%s accounts were" % rows_updated
+        self.message_user(request, "%s successfully activated." % message_bit)
+            
+    activate_users.short_description = "Activate selected accounts"
+        
+    def deactivate_users(self, request, queryset):
+        rows_updated = queryset.update(is_active=False)
+        message_bit = "1 account was" if rows_updated == 1 else "%s accounts were" % rows_updated
+        self.message_user(request, "%s successfully deactivated." % message_bit)
+    deactivate_users.short_description = "Deactivate selected accounts"
+
+
 admin.site.unregister(User)
-admin.site.register(CustomUser, UserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
 
 
 # register models in admin's site
