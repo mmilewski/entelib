c836ac9fd3c6ec5c3238c351c54a22594f976ce7
mmilewski@gmail.com <mmilewski@gmail.com@b7c1d20a-2643-11df-8747-af52ddc49692>
2010-06-09 18:48:29 +0000

    Fix csrf_token_hack ttag. Checked with Django 1.2.1 and it works.

--- entelib/baseapp/templatetags/csrf_token_hack.py
+++ entelib/baseapp/templatetags/csrf_token_hack.py
@@ -2,9 +2,10 @@ from django import template, VERSION
 
 # see http://docs.djangoproject.com/en/dev/howto/custom-template-tags/
 
+register = template.Library()
+
 # if django version is less then 1.2.x then we define a csrf_token
 if VERSION[0] == 1 and VERSION[1] < 2:
-    register = template.Library()
 
     class GoCsrfTokenNode(template.Node):
         def render(self, context):
