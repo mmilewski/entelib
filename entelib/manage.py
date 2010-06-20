78668e087f94e3c540ce67c5fb6ebaab80640d5d
mmilewski@gmail.com <mmilewski@gmail.com@b7c1d20a-2643-11df-8747-af52ddc49692>
2010-06-20 12:22:21 +0000

    Added support for injecting django 1.2.1 and imaging

--- entelib/manage.py
+++ entelib/manage.py
@@ -1,4 +1,44 @@
 #!/usr/bin/python
+
+# inject specific version of Django and Imaging (PIL)
+# Where should be Django-1.2.1? Here is dump of  tree -L 2
+# .
+# |-- entelib
+# |   |-- __init__.py
+# |   |-- auth_backends.py
+# |   |-- baseapp
+# |   |-- database
+# |   |-- dbconfigfiller.py
+# |   |-- dbcreator.py
+# |   |-- dbfiller.py
+# |   |-- email
+# |   |-- manage.py
+# |   |-- media
+# |   |-- run.sh
+# |   |-- settings.py
+# |   |-- shell.sh
+# |   |-- templates
+# |   `-- urls.py
+# `-- libs
+#     |-- Django-1.2.1
+#     |   |-- (...)
+#     |   `-- django
+#     `-- Imaging-1.1.7
+#     |   |-- (...)
+#     |   `-- PIL
+
+from os.path import abspath, dirname, join, exists
+import sys
+libs_path = join(dirname(abspath(__file__)),'..','libs')
+django_path = join(libs_path,'Django-1.2.1')
+imaging_path = join(libs_path,'Imaging-1.1.7')
+print 'Special Django path %s' % ('was found' if exists(django_path) else "wasn't found at " + django_path)
+print 'Special Imaging path %s' % ('was found' if exists(imaging_path) else "wasn't found at " + imaging_path)
+sys.path.insert(0, imaging_path)
+sys.path.insert(0, django_path)
+
+
+# original content
 from django.core.management import execute_manager
 try:
     import settings  # Assumed to be in the same directory.
