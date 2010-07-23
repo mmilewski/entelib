7353032d026a0d720b895f1bc9f93c7b624a3675
brzoskamarek <brzoskamarek@b7c1d20a-2643-11df-8747-af52ddc49692>
2010-07-23 09:48:42 +0000

    cosmetic for me to work

--- entelib/settings.py
+++ entelib/settings.py
@@ -107,7 +107,7 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 MESSAGE_LEVEL = messages.DEBUG
 
 # debug toolbar's settings -- see  http://github.com/robhudson/django-debug-toolbar
-INTERNAL_IPS = ('127.0.0.1', '10.154.4.75', )
+INTERNAL_IPS = ('127.0.0.1', '10.154.4.75', '10.154.7.179', )
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
     'SHOW_TOOLBAR_CALLBACK': None,
