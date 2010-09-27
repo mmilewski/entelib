b88bc711821b1c79ec6f475bcd9ccc3a2959c6f5
brzoskamarek <brzoskamarek@b7c1d20a-2643-11df-8747-af52ddc49692>
2010-09-27 09:56:37 +0000

    trigger alpha version

--- entelib/baseapp/sql/custom.sql
+++ entelib/baseapp/sql/custom.sql
@@ -1,1 +1,18 @@
 ALTER TABLE auth_user ADD CONSTRAINT email_must_be_unique UNIQUE (email);
+
+
+CREATE FUNCTION active_user_will_not_await_activation () RETURNS opaque AS '
+BEGIN
+    IF NEW.is_active THEN
+        UPDATE baseapp_userprofile SET awaits_activation = FALSE 
+        WHERE user_id = NEW.id;
+    END IF;
+    RETURN NEW;
+END;
+' LANGUAGE 'plpgsql';
+
+
+CREATE TRIGGER user_activated
+    AFTER INSERT OR UPDATE
+    ON auth_user FOR EACH ROW
+    EXECUTE PROCEDURE active_user_will_not_await_activation();
